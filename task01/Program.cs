b8e35b13d9b0e240feb7b95942ef97d9c91436a8
Olga <sinickaya1986@gmail.com>
2023-04-29 15:30:41 +0300

    задача на вывод числа

---
+++ task01/Program.cs
@@ -0,0 +1,5 @@
+﻿// напишите программу которая на вход принимает 
+//трехзначное число, а на выход показывает второе
+System.Console.WriteLine("Введите  число от 100 до 999 ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(num / 10 % 10);
