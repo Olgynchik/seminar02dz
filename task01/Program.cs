3ebdaa42e67150c1b7d72d85558daa3ecd24e6c4
Olga <sinickaya1986@gmail.com>
2023-04-29 15:38:37 +0300

    Добавили else

--- task01/Program.cs
+++ task01/Program.cs
@@ -2,4 +2,11 @@
 //трехзначное число, а на выход показывает второе
 System.Console.WriteLine("Введите  число от 100 до 999 ");
 int num = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(num / 10 % 10);
+if (num > 99 && num < 999)
+{
+    System.Console.WriteLine(num / 10 % 10);
+}
+else
+{
+    System.Console.WriteLine("Введите трех значное число");
+}
