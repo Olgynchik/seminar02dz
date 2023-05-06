7cd1f9b317121ee2cfdbdc14f060198ea52c2663
Olga <sinickaya1986@gmail.com>
2023-05-06 21:21:53 +0300

    нахождение числа в 3 степени

---
+++ task03/Program.cs
@@ -0,0 +1,9 @@
+﻿// напиши программу которая принимает на вход число n 
+//и выдает таблицу кубов чисел от 1 до n
+System.Console.WriteLine("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for( int ind = 1; ind < N; ind++)
+{
+    System.Console.Write($" {Math.Pow(ind,3)}"); 
+}
